ba60d5708c681bf1605fdc7aa87d7914506bbb28
Pedro Oliveira <pedrogagodev@gmail.com>
2025-02-15 18:38:53 -0400

    feat: create PUT method to change title and description of any task

--- src/routes.js
+++ src/routes.js
@@ -35,6 +35,36 @@ export const routes = [
 		},
 	},
 	{
+		method: "PUT",
+		path: buildRoutePath("/tasks/:id"),
+		handler: (req, res) => {
+			const { id } = req.params;
+			const { title, description } = req.body;
+
+			if (!title && !description) {
+				return res
+					.writeHead(400)
+					.end(
+						JSON.stringify({ message: "title or description are required" }),
+					);
+			}
+
+			const [task] = database.select("tasks", { id });
+
+			if (!task) {
+				return res.writeHead(404).end();
+			}
+
+			database.update("tasks", id, {
+				title: title ?? task.title,
+				description: description ?? task.description,
+				updated_at: new Date().toLocaleDateString(),
+			});
+
+			return res.writeHead(204).end();
+		},
+	},
+	{
 		method: "DELETE",
 		path: buildRoutePath("/tasks/:id"),
 		handler: (req, res) => {
